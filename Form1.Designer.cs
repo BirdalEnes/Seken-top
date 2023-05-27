64ec17ca90d40a2caf90f6b2aad1527fc58662da
Enes Birdal ÇAVUŞ <Enes Birdal AVU@BUKET>
2023-05-27 22:23:20 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Seken_top
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Ball = new System.Windows.Forms.Button();
+            this.Wall = new System.Windows.Forms.Panel();
+            this.Tick = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.Color.DarkMagenta;
+            this.Ball.FlatAppearance.BorderSize = 0;
+            this.Ball.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Ball.Location = new System.Drawing.Point(12, 92);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(35, 35);
+            this.Ball.TabIndex = 0;
+            this.Ball.UseVisualStyleBackColor = false;
+            this.Ball.Click += new System.EventHandler(this.Tick_Tick);
+            // 
+            // Wall
+            // 
+            this.Wall.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Wall.Location = new System.Drawing.Point(240, 150);
+            this.Wall.Name = "Wall";
+            this.Wall.Size = new System.Drawing.Size(10, 200);
+            this.Wall.TabIndex = 1;
+            // 
+            // Tick
+            // 
+            this.Tick.Enabled = true;
+            this.Tick.Interval = 1;
+            this.Tick.Tick += new System.EventHandler(this.Tick_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(500, 500);
+            this.Controls.Add(this.Wall);
+            this.Controls.Add(this.Ball);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Ball;
+        private System.Windows.Forms.Panel Wall;
+        private System.Windows.Forms.Timer Tick;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Seken_top
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int DikeyYon = 1, YatayYon = 1;
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Tick_Tick(object sender, EventArgs e)
+        {
+            if ((Ball.Bottom >= this.Height) || Ball.Top <= 0)
+            {
+                DikeyYon *= -1;
+            }
+
+            if ((Ball.Right >= this.Width) || Ball.Left <= 0)
+            {
+                YatayYon *= -1;
+            }
+
+            Ball.Top += DikeyYon;
+            Ball.Left += YatayYon;
+
+            //WALL
+
+            //DİKEY
+            if (Ball.Bottom >= Wall.Top && Ball.Top <= Wall.Bottom)
+            {
+                if (YatayYon == 1 && Ball.Right == Wall.Left)
+                {
+                    YatayYon *= -1;
+                }
+
+                if (YatayYon == -1 && Ball.Left == Wall.Right)
+                {
+                    YatayYon *= -1;
+                }
+            }
+
+
+            //YATAY
+
+            if (Ball.Right >= Wall.Left && Ball.Left <= Wall.Right)
+            {
+                if (DikeyYon == 1 && Ball.Bottom == Wall.Top)
+                {
+                    DikeyYon *= -1;
+                }
+
+                if (DikeyYon == -1 && Ball.Top == Wall.Bottom)
+                {
+                    DikeyYon *= -1;
+                }
+            }
+        }
+    }
+}
